380a07f48a0e56bb27a08bbd343331ede6f2b3e4
Mardonbek Sobirov <smardonbek9799@gmail.com>
2022-09-26 14:39:07 +0500

    Signed-off-by: Mardonbek Sobirov <smardonbek9799@gmail.com>

---
+++ main.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Sep 13 16:24:53 2022
+
+@author: Mardonbek
+"""
+
+import cx_Oracle
+
+dsn = cx_Oracle.makedsn('localhost', '8080', 'xe')
+connection = cx_Oracle.connect('Mardonbek', '9799', dsn)
+cursor = connection.cursor() 
